6f233e3a477135a2344daa02f634c37608fd056b
Alan Saunders <alan_saunders@outlook.com>
2026-01-24 00:06:44 +0930

    Further development work, nothing of note as of yet.

--- helper/csv_helper.py
+++ helper/csv_helper.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 
 __author__ = "Alan Saunders"
-__purpose__ = "Allows you to modify the RingCentral Global Shared Address book export. "
+__purpose__ = "Allows you to modify the RingCentral Global Shared Address book export. The script will automatically format the output csv to match what is expected by RingCentral. "
 __version__ = "0.1"
 __github__ = "https://github.com/Ripped-Kanga/RingCentral-CSV-Editor\n"
-__disclaimer__ = ""
-
 
 # Import Libraries
 import os
 import csv
 from datetime import datetime
+import pprint
 
 
 # Helper to ingest the RingCentral Global Shared address book csv file. 
@@ -25,12 +24,44 @@ class RingCentralCSV:
 			print(f"/{self.csv_path_out} does not exist, creating...")
 			os.makedirs(csv_path_out, exist_ok=True)
 
-	def _test(self, name):
-		date = datetime.now().strftime("%y-%m-%d")
-		return (f"{name} - {date}")
 
-	def csv_reader(self, csv_in_path):
-		with open(csv_in_path, newline='') as csvfile:
-		    address_book = csv.reader(csvfile, delimiter=' ', quotechar='|')
-		    for row in address_book:
-		        print(', '.join(row))
+	'''
+	Checks the incoming csv file to see if it has RingCentral junk comment data. If it does
+	strip the junk data out and write a new dict starting from row 24.
+	If it doesn't, ingest the csv, checking that the headers are correct.
+	'''
+	def checker(self, csv_in_path):
+		with open(csv_in_path, newline="", encoding="utf-8") as f:
+			first_line = f.readline()
+			check_junk = (first_line or "").strip().lstrip('"')
+
+		with open(csv_in_path, newline="", encoding="utf-8") as csvfile:
+
+			if check_junk.startswith("Please follow the"):
+				header_row = 24
+				# Skip lines 1..23 as they contain junk data
+				for _ in range(header_row - 1):
+					next(csvfile, None)
+
+				reader = csv.DictReader(csvfile)  # line 24 becomes the header row
+				cleaned_rows = list(reader)
+
+				pprint.pprint (cleaned_rows, sort_dicts=False)
+				print ("Data was dirty")
+
+			elif check_junk.startswith("First Name,Surname,"):
+				print("File is clean, writing csv data to dictionary...")
+				reader = csv.DictReader(csvfile, delimiter=",")
+				data_ingest = list(reader)
+				pprint.pprint(data_ingest, sort_dicts=False)
+
+				print ("Data was clean")
+
+			else:
+				print("Error in csv_in condition logic")
+				exit()
+
+
+	def csv_writer(self, csv_out_path):
+		with open('eggs.csv', 'w', newline='') as csvfile:
+			new_address_book = csv.writer(csvfile, delimiter=',')
--- main.py
+++ main.py
@@ -8,17 +8,35 @@ __disclaimer__ = ""
 
 
 # Import Libraries
+import sys
+import os
 from helper.csv_helper import RingCentralCSV
 
 def main():
 	rc_csv = RingCentralCSV()
-	process_name = rc_csv._test("alan")
-	print (process_name)
 
+	csv_in_name = input("Specify the csv file name: ")
 	print ("Printing CSV File:\n")
 
-	rc_csv.csv_reader(input("Specify the csv file name: "))
+	rc_csv.checker(csv_in_name)
+
+	print (f"The data of {csv_in_name} is displayed above, review before continuing...")
+	csv_changes = []
+	first_name = input("What is the new contact first name? ")
+	last_name = input("What is the new contact last name? ")
+	mobile = input("What is the new contact mobile number? ")
+	csv_changes.extend([first_name,last_name,mobile])
+	print (csv_changes)
 
 
+# Start Execution
 if __name__ == "__main__":
-	main()
+	# Start main() and listen for keyboard interrupts
+	try:
+		main()
+	except KeyboardInterrupt:
+		print('\nInterrupted by keyboard CTRL + C, exiting...\n')
+		try:
+			sys.exit(130)
+		except SystemExit:
+			os._exit(130)
